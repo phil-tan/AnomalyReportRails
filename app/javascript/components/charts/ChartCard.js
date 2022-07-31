14dcdd066a1c67fe7ced3484e61f0ea3e25aa9cf
Phil Tan <philcode40@gmail.com>
2022-07-31 13:39:16 +1000

    before changing update to chart panel

--- app/javascript/components/charts/ChartCard.js
+++ app/javascript/components/charts/ChartCard.js
@@ -7,8 +7,34 @@ import EditButton from './EditButton';
 
 const ChartCard = ({ chart, charts_data, site_points_list}) => {
 
+  let chart_width = ''
+
+  switch(chart.width) {
+    case 'full':
+      chart_width = '100%'
+      break;
+    case '3/4':
+      chart_width = '75%'
+      break;
+    case '2/3':
+      chart_width = '67%'
+      break;
+    case '1/2':
+      chart_width = '50%'
+      break;
+    case '1/3':
+      chart_width = '33%'
+      break;
+    case '1/4':
+      chart_width = '25%'
+      break;
+    default:
+      chart_width = '50%'
+  }
+
+
   return (
-    <div className='chart-card-wrap'>
+    <div className='chart-card-wrap' style={{width: chart_width}}>
     <div className='chart-card'>
       <div className='chart-menu'>
         <div><strong>{chart.title}</strong></div>
