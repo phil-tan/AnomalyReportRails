40650dac14db5aad778e9c106dc151a68b452782
Phil Tan <philcode40@gmail.com>
2022-08-05 12:17:12 +1000

    multiplier works

--- app/javascript/components/charts/KpiContent.js
+++ app/javascript/components/charts/KpiContent.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 const KpiContent = ({ chart, charts_data }) => {
 
-
   const points = chart.points.split(',')
   const plot_options = chart.plot_options ? chart.plot_options.split(',') : []
   let opt_hash = {}
@@ -11,21 +10,25 @@ const KpiContent = ({ chart, charts_data }) => {
       opt_hash[opt.split('=')[0]] = opt.split('=')[1]
     })
   }
-
+  console.log(opt_hash)
   let value;
   let value_text;
   let text_color;
+  const multiplier = opt_hash['multiplier'] ? parseFloat(opt_hash['multiplier']) : 1
+  const multiplier_2 = opt_hash['multiplier-2'] ? parseFloat(opt_hash['multiplier-2']) : 1
+
+
   if(chart.plot_type==='kpi' && charts_data){
     console.log('entered kpi sum content calc')
     if(points[0] in charts_data){
-      value = charts_data[points[0]].reduce((a,b) => a + b, 0)
+      value = multiplier*charts_data[points[0]].reduce((a,b) => a + b, 0)
     }
-    value_text = Math.round(value).toLocaleString('en-US')
+    value_text = !isNaN(value) ? Math.round(value).toLocaleString('en-US') : ''
   }else if(chart.plot_type==='kpi-diff' && charts_data){
     console.log(points)
     if(points[0] in charts_data && points[1] in charts_data){
-      const sum1 = charts_data[points[0]].reduce((a,b) => a + b, 0)
-      const sum2 = charts_data[points[1]].reduce((a,b) => a + b, 0)
+      const sum1 = multiplier*charts_data[points[0]].reduce((a,b) => a + b, 0)
+      const sum2 = multiplier_2*charts_data[points[1]].reduce((a,b) => a + b, 0)
       value = (100*(sum1 - sum2)/sum2)
     }
     if(value < 0){
@@ -34,7 +37,7 @@ const KpiContent = ({ chart, charts_data }) => {
       text_color = opt_hash['pos_color']
     }
 
-    value_text = Math.round(value).toLocaleString('en-US') + '%';
+    value_text = !isNaN(value) ? Math.round(value).toLocaleString('en-US') + '%' : ''
     value_text = (value > 0) ? "+" + value_text : value_text
     text_color = text_color ? text_color : ''
   }
